f2e70edb964500b7fb9c2ae6a6f3255f37ca5611
Penny Chen <penny@andestech.com>
2015-01-12 10:56:49 +0800

    update version "Andes ICEman v3.1.0"

--- iceman_adapter/iceman_adapter.c
+++ iceman_adapter/iceman_adapter.c
@@ -170,7 +170,7 @@ extern int openocd_main(int argc, char *argv[]);
 extern char *nds32_edm_passcode_init;
 
 static void show_version(void) {
-	printf("Andes ICEman v3.0.0 (OpenOCD) BUILD_ID: %s\n", BUILD_ID);
+	printf("Andes ICEman v3.1.0 (OpenOCD) BUILD_ID: %s\n", BUILD_ID);
 	printf("Copyright (C) 2007-2014 Andes Technology Corporation\n");
 }
 
@@ -905,7 +905,7 @@ int main(int argc, char **argv) {
 		printf("tcl port num error\n");
 		return 0;
 	}
-	printf("Andes ICEman v3.0.0 (OpenOCD) BUILD_ID: %s\n", BUILD_ID);
+	printf("Andes ICEman v3.1.0 (OpenOCD) BUILD_ID: %s\n", BUILD_ID);
 	printf("Burner listens on %d\n", burner_port);
 	printf("Telnet port: %d\n", telnet_port);
 	printf("TCL port: %d\n", tcl_port);
