cf80f5f155aae6accb4af3a870415b0ec67c8d44
Hellosun Ji-Heng Wu <hellosun@andestech.com>
2015-02-10 18:25:48 +0800

    nds32: add missing aice_adapter/config.h

---
+++ aice_adapter/config.h
@@ -0,0 +1,27 @@
+#ifndef __CONFIG_H_
+#define __CONFIG_H_ 
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <unistd.h>
+#include "aice_log.h"
+
+#define LOG_ERROR   aice_log_error
+#define LOG_DEBUG   aice_log_debug 
+#define LOG_INFO    aice_log_info 
+#define LOG_WARNING aice_log_error
+
+#define ERROR_OK (0)
+#define ERROR_FAIL (-4)
+
+#include <errno.h>
+
+#ifndef ETIMEDOUT
+#define ETIMEDOUT WSAETIMEDOUT
+#endif
+
+
+
+#endif 
