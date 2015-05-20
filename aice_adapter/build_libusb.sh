502a432f236ecb4f1854f87b10641b7ede9ef181
Hellosun Wu <hellosun@andestech.com>
2015-05-20 20:11:56 +0800

    aice_adapter build single alone
    Step 1: ./build_libusb
    Step 2: make -f Makefile_single

---
+++ aice_adapter/build_libusb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+#Clean 
+rm -rf libusb 
+
+UNAMESTR=`uname`
+BUILD_DIR=`readlink -f ./`
+
+#Install libusb 
+mkdir -p ./libusb
+if [[ "$UNAMESTR" == 'Linux' ]]; then
+    # build libusb-1.0
+    cd ./libusb-1.0.18 
+    ./configure --prefix=$BUILD_DIR/libusb CFLAGS='-g3 -O0' --disable-shared --disable-udev --disable-timerfd
+    make install -j8
+    rm -rf $BUILD_DIR/libusb/libusb-config
+else
+    # build libusb-win32
+    cd ./libusb-win32-bin-1.2.6.0 
+    ./install.sh $BUILD_DIR/libusb
+fi
+
