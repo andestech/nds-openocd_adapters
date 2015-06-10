26db35cf53d0e6abb016b5775975883db22a61c6
Hellosun Wu <hellosun@andestech.com>
2015-06-10 14:16:48 +0800

    Make aice_adapter build in x86 only

--- aice_adapter/build_libusb.sh
+++ aice_adapter/build_libusb.sh
@@ -11,7 +11,7 @@ mkdir -p ./libusb
 if [[ "$UNAMESTR" == 'Linux' ]]; then
     # build libusb-1.0
     cd ./libusb-1.0.18 
-    ./configure --prefix=$BUILD_DIR/libusb CFLAGS='-g3 -O0' --disable-shared --disable-udev --disable-timerfd
+    ./configure --prefix=$BUILD_DIR/libusb CFLAGS='-m32 -g3 -O0' CXXFLAGS='-m32' LDFLAGS='-m32' --disable-shared --disable-udev --disable-timerfd
     make install -j8
     rm -rf $BUILD_DIR/libusb/libusb-config
 else
