b108fb9eb3bcedd5e0040c28ee945e13ff1831d0
Penny Chen <penny@andestech.com>
2016-03-25 10:07:03 +0800

    update version "Andes ICEman v3.2.4"

--- iceman_adapter/iceman_adapter.c
+++ iceman_adapter/iceman_adapter.c
@@ -21,7 +21,7 @@
 #define PORTNUM_GDB        1111
 
 #define LINE_BUFFER_SIZE   2048
-#define ICEMAN_VERSION     "v3.2.3"
+#define ICEMAN_VERSION     "v3.2.4"
 #define NDS32_USER_CFG     "nds32_user.cfg"
 
 #ifdef __GNUC__
