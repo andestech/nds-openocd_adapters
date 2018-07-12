cee4e9d6c597bebce472f70c8faf96a99d2e1a1e
Hellosun Wu <wujiheng.tw@gmail.com>
2018-07-12 10:33:08 +0800

    ndsv5: Modify example_custom script(For V5 example)

--- iceman_adapter/example_custom.tcl
+++ iceman_adapter/example_custom.tcl
@@ -1,6 +1,5 @@
-set_trst 500
-clear_trst 500
-set_srst 500
-clear_srst 500
-#write_pins 02310234567
-delat 500
+set_srst 256      #Drive SRSTn to LOW and pause for 256 milliseconds
+set_dbgi 72       #Drive DBGI_N to LOW and pause for 72 milliseconds
+clear_dbgi 10     #Drive DBGI_N to HIGH and pause for 10 milliseconds
+clear_srst 500    #Drive SRSTn to HIGH and pause for 500 milliseconds
+
