a7e944ccd435b47ab15eda29f9d8611f881367ac
Hellosun Wu <wujiheng.tw@gmail.com>
2018-07-17 19:10:43 +0800

    ndsv5: Add request msg in ICEman.sh when remove ftdi_sio

--- iceman_adapter/ICEman.sh
+++ iceman_adapter/ICEman.sh
@@ -16,8 +16,27 @@ if [ "$?" != "0" ]; then
 	exit 4
 fi
 
-rmmod ftdi_sio
-echo 'blacklist ftdi_sio' >> /etc/modprobe.d/blacklist
-echo 'blacklist ftdi_sio' >> /etc/modprobe.d/blacklist.conf
+function func_yes {
+	rmmod ftdi_sio 2> /dev/null
+	echo 'blacklist ftdi_sio' >> /etc/modprobe.d/blacklist 2> /dev/null
+	echo 'blacklist ftdi_sio' >> /etc/modprobe.d/blacklist.conf 2> /dev/null
+}
+
+function func_no {
+	echo "Please issue the following commands before executing ICEman: sudo rmmod ftdi_sio"
+}
+
+echo -e "The kernel module ftdi_sio will be removed and added to the modprob blacklist.\n\
+This will stop the ftdi_sio module from being installed when the FTDI USB serial device converter is detected and\n\
+ensure ICEman to execute correctly.\n\
+Are you sure you want to continue? (Please enter a number)"
+select yn in "Yes" "No"; do
+	case $yn in
+		Yes ) func_yes; break;;
+		No  ) func_no;  exit;;
+		*   ) echo "Please type a number corresponding to your choice!"
+	esac
+done
 
 echo "Done!"
+
